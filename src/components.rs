c54960ffc79521ac9a4e2f1451869bc496dc063a
Ellie Frost <web@stillinbeta.com>
2019-04-29 16:40:40 -0400

    Program picker, formatting, rendering

--- src/components.rs
+++ src/components.rs
@@ -1,6 +1,7 @@
 use yew::html;
-use yew::html::{Component, ComponentLink, Html, Renderable, ShouldRender};
-use zeerust::ops::Op;
+use yew::html::{ChangeData, Component, ComponentLink, Html, Renderable, ShouldRender};
+use yew::Callback;
+use zeerust::ops::{Op, Reg16, Reg8};
 
 pub struct Opcode {
     opcode: Option<Op>,
@@ -8,7 +9,7 @@ pub struct Opcode {
 
 #[derive(PartialEq, Default, Clone)]
 pub struct OpcodeProperties {
-    opcode: Option<Op>,
+    pub opcode: Option<Op>,
 }
 
 impl Component for Opcode {
@@ -43,5 +44,208 @@ impl Renderable<Opcode> for Opcode {
             }
             </div>
         )
+    }
+}
+
+#[derive(Default, PartialEq, Clone)]
+pub struct Registers {
+    pub registers: zeerust::cpu::reg::Registers,
+}
+
+impl Registers {
+    fn make_pair(&self, a: Reg8, b: Reg8, ab: Reg16) -> Html<Registers> {
+        html! {
+            <>
+            <Register16: label={ format!("{}", ab) }, value = self.registers.get_reg16(&ab), />
+            <tr>
+                <Register8: label = { format!("{}", a) }, value = self.registers.get_reg8(a), />
+                <Register8: label = { format!("{}", b) }, value = self.registers.get_reg8(b), />
+            </tr>
+            </>
+        }
+    }
+}
+
+impl Component for Registers {
+    type Message = ();
+    type Properties = Self;
+
+    fn create(props: Self::Properties, _link: ComponentLink<Self>) -> Self {
+        props
+    }
+
+    fn change(&mut self, props: Self::Properties) -> ShouldRender {
+        self.registers = props.registers;
+        true
+    }
+
+    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
+        false
+    }
+}
+
+impl Renderable<Registers> for Registers {
+    fn view(&self) -> Html<Registers> {
+        html! {
+            <table>
+              { self.make_pair(Reg8::A, Reg8::F, Reg16::AF) }
+              { self.make_pair(Reg8::B, Reg8::C, Reg16::BC,) }
+              { self.make_pair(Reg8::D, Reg8::E, Reg16::DE) }
+              { self.make_pair(Reg8::H, Reg8::L, Reg16::HL) }
+              <Register16: label={"PC"}, value = self.registers.get_pc(), />
+            </table>
+        }
+    }
+}
+
+#[derive(Clone, PartialEq, Default)]
+struct RegisterPair {
+    a: u8,
+    aname: String,
+    b: u8,
+    bname: String,
+}
+
+impl Component for RegisterPair {
+    type Message = ();
+    type Properties = Self;
+
+    fn create(props: Self::Properties, _link: ComponentLink<Self>) -> Self {
+        props
+    }
+
+    fn change(&mut self, props: Self::Properties) -> ShouldRender {
+        // Never update the names
+        self.a = props.a;
+        self.b = props.b;
+        true
+    }
+
+    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
+        false
+    }
+}
+
+#[derive(Default, PartialEq, Clone)]
+struct Register16 {
+    pub label: String,
+    pub value: u16,
+}
+
+impl Component for Register16 {
+    type Message = ();
+    type Properties = Self;
+
+    fn create(props: Self::Properties, _link: ComponentLink<Self>) -> Self {
+        props
+    }
+
+    fn change(&mut self, props: Self::Properties) -> ShouldRender {
+        self.value = props.value;
+        true
+    }
+
+    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
+        false
+    }
+}
+
+impl Renderable<Register16> for Register16 {
+    fn view(&self) -> Html<Register16> {
+        html! {
+        <tr>
+            <td colspan="2",>
+                <strong> { &self.label }{ ":" } </strong>
+                <code> { format!("{:04x}", self.value) }</code>
+            </td>
+        </tr>
+        }
+    }
+}
+
+#[derive(Default, PartialEq, Clone)]
+struct Register8 {
+    pub label: String,
+    pub value: u8,
+}
+
+impl Component for Register8 {
+    type Message = ();
+    type Properties = Self;
+
+    fn create(props: Self::Properties, _link: ComponentLink<Self>) -> Self {
+        props
+    }
+
+    fn change(&mut self, props: Self::Properties) -> ShouldRender {
+        self.value = props.value;
+        true
+    }
+
+    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
+        false
+    }
+}
+
+impl Renderable<Register8> for Register8 {
+    fn view(&self) -> Html<Register8> {
+        html! {
+                <td>
+                <strong> { &self.label }{ ":" } </strong>
+                <code> { format!("{:02x}", self.value) }</code>
+                </td>
+        }
+    }
+}
+
+#[derive(PartialEq, Clone, Default)]
+pub struct ProgramSelect {
+    pub onchange: Option<Callback<&'static [u8]>>,
+}
+
+impl Component for ProgramSelect {
+    type Message = usize;
+    type Properties = Self;
+
+    fn create(props: Self::Properties, _link: ComponentLink<Self>) -> Self {
+        props
+    }
+
+    fn change(&mut self, props: Self::Properties) -> ShouldRender {
+        self.onchange = props.onchange;
+        true
+    }
+
+    fn update(&mut self, msg: usize) -> ShouldRender {
+        if let Some(ref mut callback) = self.onchange {
+            if let Some(example) = zeerust::examples::EXAMPLES.get(msg) {
+                callback.emit(example.binary)
+            }
+        }
+        true
+    }
+}
+
+fn program_option(opts: (usize, &str)) -> Html<ProgramSelect> {
+    let (index, name) = opts;
+    html! {
+        <option value=index, >{ name }</option>
+    }
+}
+
+impl Renderable<ProgramSelect> for ProgramSelect {
+    fn view(&self) -> Html<ProgramSelect> {
+        html! {
+            <select onchange=|evt| {
+                match evt {
+                    ChangeData::Select(elem) => {
+                        elem.selected_index().map(|x| x as usize).unwrap()
+                    },
+                    _ => unimplemented!("reached unknown location"),
+                }
+            }, >
+                { for zeerust::examples::EXAMPLES.iter().map(|e| e.name).enumerate().map(program_option) }
+            </select>
+        }
     }
 }
--- src/model.rs
+++ src/model.rs
@@ -2,15 +2,61 @@ use yew::html;
 use yew::html::{Component, ComponentLink, Html, Renderable, ShouldRender};
 use zeerust::z80::Z80;
 
-use crate::components::Opcode;
+use crate::components::{Opcode, ProgramSelect, Registers};
 
 pub struct Model {
     z80: Z80<'static>,
+    show_memory: bool,
 }
 
 pub enum CPUCommand {
     Step,
     Run,
+    ShowMemory(bool),
+    LoadProgram(&'static [u8]),
+}
+
+fn byte_view(byte: (usize, &u8)) -> Html<Model> {
+    let (index, byte) = byte;
+    let b = format!("{:02x} ", byte);
+    let sp = match index + 1 {
+        i if i % 20 == 0 => "\n",
+        i if i % 10 == 0 => "   ",
+        _ => " ",
+    };
+    let addr = format!("{:04x}", index);
+    html! {<><a id={ &addr }, title={ &addr },>{ b }</a>{ sp }</>}
+}
+
+impl Model {
+    fn memory_view(&self) -> Html<Self> {
+        html! {
+            <pre>
+            { for self.z80.memory.memory.iter().enumerate().map(byte_view) }
+            </pre>
+        }
+    }
+
+    fn memory_ui(&self) -> Html<Self> {
+        if self.show_memory {
+            html! {
+                <>
+                <button onclick=|_| CPUCommand::ShowMemory(false),>
+                    { "Hide Memory" }
+                </button>
+                { self.memory_view() }
+                </>
+            }
+        } else {
+            html! {
+                <>
+                <button onclick=|_| CPUCommand::ShowMemory(true),>
+                    { "Show Memory " }
+                </button>
+                </>
+            }
+        }
+    }
 }
 
 impl Component for Model {
@@ -20,6 +66,7 @@ impl Component for Model {
     fn create(_props: Self::Properties, _link: ComponentLink<Self>) -> Self {
         Self {
             z80: Z80::default(),
+            show_memory: false,
         }
     }
 
@@ -27,6 +74,8 @@ impl Component for Model {
         match msg {
             CPUCommand::Step => self.z80.step(),
             CPUCommand::Run => self.z80.run(),
+            CPUCommand::ShowMemory(b) => self.show_memory = b,
+            CPUCommand::LoadProgram(program) => self.z80.load(program),
         }
         true
     }
@@ -40,11 +89,18 @@ impl Renderable<Model> for Model {
             .map(|(opc, _consumed)| opc);
 
         html! {
-            <div>
-                <Opcode: opcode=opcode, />
+            <content>
+                <div>
+                    <Opcode: opcode=opcode, />
+                </div>
+                <div>
+                    <Registers: registers=self.z80.registers.clone(), />
                 </div>
                 <button onclick=|_| CPUCommand::Step,> { "Step" }  </button>
                 <button onclick=|_| CPUCommand::Run,> { "Run" } </button>
+                { self.memory_ui() }
+                <ProgramSelect: onchange=|program| CPUCommand::LoadProgram(program), />
+            </content>
         }
     }
 }
